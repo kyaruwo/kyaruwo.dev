76d762cf09ec00206a8e3e9cea6d7f43a8375677
Dv <75692623+kyaruwo@users.noreply.github.com>
2025-01-09 17:57:30 +0800

    /

---
+++ postbuild.js
@@ -0,0 +1,18 @@
+import { unlinkSync } from "fs";
+import { replaceInFileSync } from "replace-in-file";
+
+const build = "../_builds/kyaruwo.cloud/build";
+
+try {
+  unlinkSync(build + "/_app/env.js");
+  console.log("rem: env.js");
+
+  replaceInFileSync({
+    files: build + "/404.html",
+    from: /"\.\//g,
+    to: '"/',
+  });
+  console.log("rei: 404.html");
+} catch (error) {
+  console.error("Error occurred:", error);
+}
