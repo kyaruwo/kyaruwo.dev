36161ff0ce535e1c903aa1b491cd67fafa800851
Dv <75692623+kyaruwo@users.noreply.github.com>
2025-01-09 20:36:55 +0800

    update 404

--- src/routes/404/+page.svelte
+++ src/routes/404/+page.svelte
@@ -3,3 +3,8 @@
 </script>
 
 <Navigation />
+
+<div class="flex flex-col items-center gap-4 pt-40">
+  <span class="text-9xl font-black">404</span>
+  <p class="text-5xl font-medium">Not Found</p>
+</div>
