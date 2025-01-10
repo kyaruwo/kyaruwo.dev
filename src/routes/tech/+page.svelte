7fa53eef4841e2dd495e743d1474174ca18fbea9
Dv <75692623+kyaruwo@users.noreply.github.com>
2025-01-11 03:27:41 +0800

    update tech

--- src/routes/tech/+page.svelte
+++ src/routes/tech/+page.svelte
@@ -1,5 +1,100 @@
 <script>
   import Navigation from "$lib/Navigation.svelte";
+
+  const stackoyaki = [
+    { name: "tailwindcss", description: "style" },
+    { name: "svelte", description: "frontend" },
+    { name: "caddy", description: "server" },
+    { name: "go", description: "backend" },
+    { name: "sqlite", description: "database" },
+  ];
+
+  const technologies = [
+    "porkbun",
+    "cloudflare",
+    "github",
+    "githubdesktop",
+    "vscode",
+    "alacritty",
+    "syncthing",
+    "firefox",
+    "discord",
+    "godot",
+    "debian",
+    "android",
+    "windows",
+    "termux",
+    "tmux",
+    "caddy",
+    "go",
+    "sqlite",
+    "json",
+    "markdown",
+    "nodejs",
+    "npm",
+    "svelte",
+    "tailwindcss",
+    "javascript",
+    "html",
+    "rust",
+    "postgresql",
+    "mysql",
+    "python",
+  ];
 </script>
 
 <Navigation />
+
+<div class="flex flex-col gap-24">
+  <!-- start of stackoyaki -->
+  <div class="flex flex-col gap-8">
+    <!-- start of stackoyaki label -->
+    <div class="flex flex-col items-center">
+      <span class="text-4xl font-bold underline">stackoyaki</span>
+      <p class="text-zinc-400">web development stack</p>
+    </div>
+    <!-- end of stackoyaki label -->
+    <!-- start of stackoyaki icons -->
+    <div class="flex flex-row justify-evenly sm:mx-10 md:mx-20 lg:mx-40">
+      {#each stackoyaki as tech}
+        <div class="flex flex-col items-center">
+          <img
+            class="size-12 select-none sm:size-16 md:size-20 lg:size-24"
+            draggable="false"
+            src="/tech-icons/{tech.name}.svg"
+            alt={tech.name}
+          />
+          <p class="text-xs text-zinc-400 sm:text-sm md:text-base lg:text-lg">
+            {tech.description}
+          </p>
+        </div>
+      {/each}
+    </div>
+    <!-- end of stackoyaki icons -->
+  </div>
+  <!-- end of stackoyaki -->
+  <!-- start of technologies -->
+  <div class="flex flex-col gap-8">
+    <!-- start of technologies label -->
+    <div class="flex flex-col items-center">
+      <span class="text-4xl font-bold underline">technologies</span>
+      <p class="text-zinc-400">I'm familiar with</p>
+    </div>
+    <!-- end of technologies label -->
+    <!-- start of technologies icons -->
+    <div
+      class="grid grid-cols-5 gap-4 sm:mx-10 sm:grid-cols-10 md:mx-20 lg:mx-40"
+    >
+      {#each technologies as tech}
+        <img
+          class="size-10 select-none justify-self-center md:size-12 lg:size-14"
+          draggable="false"
+          src="/tech-icons/{tech}.svg"
+          alt={tech}
+        />
+      {/each}
+    </div>
+    <!-- end of technologies icons -->
+  </div>
+  <!-- end of technologies -->
+</div>
