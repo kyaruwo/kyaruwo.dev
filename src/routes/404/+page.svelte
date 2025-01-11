99d88970ebfcafe7de9944541c9c6c947f9f3f6e
Dv <75692623+kyaruwo@users.noreply.github.com>
2025-01-12 02:10:14 +0800

    404 focused

--- src/routes/404/+page.svelte
+++ src/routes/404/+page.svelte
@@ -1,15 +1,12 @@
-<script>
-  import Header from "$lib/Header.svelte";
-  import Footer from "$lib/Footer.svelte";
-</script>
-
-<Header />
-
 <!-- start of 404 -->
 <div class="flex flex-grow flex-col items-center justify-center gap-4">
   <span class="text-9xl font-black">404</span>
   <p class="text-5xl font-medium">Not Found</p>
+  <a
+    class="my-8 rounded-lg border-2 border-zinc-400 p-4 font-bold text-zinc-400 hover:border-white hover:text-white"
+    href="/"
+  >
+    RETURN HOME
+  </a>
 </div>
 <!-- end of 404 -->
-
-<Footer />
